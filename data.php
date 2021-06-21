aeeecc3eb813a706aebd10156d971606734394e2
mk-to <85170259+mk-to@users.noreply.github.com>
2021-06-22 04:46:03 +0900

    Add files via upload

--- data.php
+++ data.php
@@ -2,9 +2,9 @@
 require_once('weapon.php')
 require_once('item.php')
 
-$sword = werpon('ironsword', 120, 'https://mk-to.github.io/php/menu.img/image/swd.jpg')
-$shield = werpon('ironshild', 150, 'https://mk-to.github.io/php/menu.img/image/shd.jpg')
-$armagh = werpon('ironarmagh', 500, 'https://mk-to.github.io/php/menu.img/image/amh.jpg')
+$sword = werpon('ironsword', 120, 'https://mk-to.github.io/php/menu.img/image/swd.jpg');
+$shield = werpon('ironshild', 150, 'https://mk-to.github.io/php/menu.img/image/shd.jpg');
+$armagh = werpon('ironarmagh', 500, 'https://mk-to.github.io/php/menu.img/image/amh.jpg');
 
 $menu = array($ironsword, $woodshild, $ironarmagh, $herb);
 
